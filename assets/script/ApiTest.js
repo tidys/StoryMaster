17d4e6f1ba78034176b8faeffc2bfced6a740378
xyf-mac <xu_yanfeng@126.com>
2020-09-06 23:37:38 +0800

    api代码示例

--- assets/script/ApiTest.js
+++ assets/script/ApiTest.js
@@ -1,13 +1,12 @@
 cc.director.on(cc.Director.EVENT_BEFORE_SCENE_LAUNCH, () => {
     cc.StoryMaster.setStoryPlotJumpCallBack((data, next) => {
         console.log(data)
-        if (data) {
-
+        if (data.tag === 'peiqi') {
+            cc.log('佩琪佩琪');
         }
         next && next();
     });
     cc.StoryMaster.onPieceTalkOver((data) => {
-        debugger
     })
 });
 
